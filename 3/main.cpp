04292b4d277c78929648bd4859219ece386ec352
Joshua A <jojo.asmo94@gmail.com>
2021-12-19 23:30:23 +0100

    Attempt solving day3p2; Attempt failed

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
 #include <math.h>
 namespace st = std;
 
 #define FILE_PATH "./data.txt"
 
-void readData(st::vector<st::string> &data) {
+void readData(st::list<st::string> &data) {
     st::ifstream f (FILE_PATH);
     st::string lineContent;
     auto index = 0;
@@ -24,12 +25,14 @@ void readData(st::vector<st::string> &data) {
     f.close();
 }
 
-void findMostCommonBit(st::vector<st::string> &data, st::vector<int> &rate)  {
-    for(auto &line: data)   {
-        for(auto i = 0; i < line.size(); i++)  {
-            char c = line[i];
-            if(c == '1') rate[i] += 1;
-            else rate[i] -= 1;
+void findMostCommonBit(st::list<st::string> &data, st::vector<int> &rate)  {
+    auto it = data.begin();
+    for(it; it != data.end(); ++it) {
+        auto binary = *it;
+        for(auto j = 0; j < binary.length(); ++j)    {
+            auto bit = binary[j];
+            if(bit == '1')  rate[j] += 1;
+            else    rate[j] -= 1;
         }
     }
     for(auto i = 0; i < rate.size(); i++) {
@@ -39,7 +42,8 @@ void findMostCommonBit(st::vector<st::string> &data, st::vector<int> &rate)  {
 }
 
 int binaryToInteger(st::vector<int> const &binary) {
-    auto sum = 0, exp = 11;
+    auto sum = 0;
+    auto exp = binary.size() - 1;
     for(auto i = 0; i < binary.size(); i++) {
         sum += binary[i] * pow(2, exp);
         --exp;
@@ -47,36 +51,38 @@ int binaryToInteger(st::vector<int> const &binary) {
     return sum;
 }
 
-st::vector<int> findBitInPlace(st::vector<st::string> data, st::vector<int> const &commonBits)    {
-    for(auto &bit: commonBits) st::cout << bit;
-    st::cout << "\n\n";
+int binaryToInteger(st::string const &binary) {
+    auto sum = 0;
+    auto exp = binary.size() - 1;
+    for(auto i = 0; i < binary.size(); i++) {
+        auto bit = (int)binary[i] - 48;                 // 48 = OFFSET for converting char to corresponding int
+        sum += bit * pow(2, exp);
+        --exp;
+    }
+    return sum;
+}
+
+st::string findMostSimilarNumber(st::list<st::string> data, st::vector<int> const &commonBits)    {
     for(auto i = 0; i < commonBits.size(); i++) {
         char cBit;
         if(commonBits[i] == 1) cBit = '1';
         else cBit = '0';
-        for(auto j = 0; j < data.size(); j++)   {
-            char dBit = data[j][i];
+        auto it = data.begin();
+        for(it; it != data.end(); ++it) {
+            auto binary = *it;
+            auto dBit = binary[i];
             if(dBit != cBit) {
-                data.erase(data.begin() + j);
+                it = data.erase(it);
+                --it;
             }
         }
         if(data.size() == 1) break;
     }
-
-    for(auto &line: data) {
-        st::cout << line << st::endl;
-    }
-
-    st::vector<int> v(12, 0);
-    for(auto i = 0; i < data[0].size(); i++)    {
-        if(data[0][i] == '0') v[i] = 0;
-        else    v[i] = 1;
-    }
-    return v;
+    return *data.begin();
 }
 
 int main()  {
-    st::vector<st::string> data;
+    st::list<st::string> data;
     readData(data);
 
     st::vector<int> gammaRate(12, 0);
@@ -87,13 +93,14 @@ int main()  {
         epsilonRate[i] = !gammaRate[i];
     }
 
-    int powerOutput = binaryToInteger(gammaRate) * binaryToInteger(epsilonRate);
-    // st::cout << "powerOutput= " << powerOutput << st::endl;
+    // int powerOutput = binaryToInteger(gammaRate) * binaryToInteger(epsilonRate);         //  uncomment to find out
+    // st::cout << "powerOutput= " << powerOutput << st::endl;                              //  answer to part 1
 
-    auto oxyGenRating = binaryToInteger(findBitInPlace(data, gammaRate));
-    // auto co2ScrubRating = binaryToInteger(findBitInPlace(data, epsilonRate));
-    // auto lifeSuppRating = oxyGenRating * co2ScrubRating;
-    // st::cout << lifeSuppRating;
+    auto oxyGenRating = binaryToInteger(findMostSimilarNumber(data, gammaRate));
+    auto co2ScrubRating = binaryToInteger(findMostSimilarNumber(data, epsilonRate));
+
+    auto lifeSuppRating = oxyGenRating * co2ScrubRating;
+    st::cout << lifeSuppRating;
 
     return 0;
 }
